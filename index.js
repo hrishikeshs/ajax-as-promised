2a12dc3381099343b657edfada8e98ea22197639
hrishikeshs <hrish2006@gmail.com>
2015-02-21 21:22:25 +0530

    Initial implementation, use bluebird for now.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+var Promise = require("bluebird");
+
 module.exports = {
-  request:  function(url, options) {
-    
+  request:  function(url, type, options) {
+    var xhr = new XMLHttpRequest();
+    return new Promise(function(resolve, reject) {
+      xhr.onload = function() {
+        if(this.status === 200) {
+          resolve(this.response);
+        }
+        else {
+          reject(this.response);
+        }
+      };
+      xhr.open(type, url);
+      xhr.send();
+    });
   }
-                           
-}
+};
