e2539930f5cc47114584d5d15c0e53ae9ed51851
hrishikeshs <hrish2006@gmail.com>
2015-03-07 14:55:17 +0530

    Update readme with a better api, tweak design.

--- get.js
+++ get.js
@@ -1,14 +1,7 @@
 var Promise = require("bluebird");
-var GET = require("get");
-/*
 
-var PUT = require("put");
-var POST = require("POST");
-var DELETE = require("DELETE");
-// more verbs to be added as basic utilities here
-*/
 module.exports = {
-  request:  function(url, type, options) {
+  get:  function(url, options) {
     var xhr = new XMLHttpRequest();
     return new Promise(function(resolve, reject) {
       xhr.onload = function() {
@@ -19,8 +12,8 @@ module.exports = {
           reject(this.response);
         }
       };
-      xhr.open(type, url);
+      xhr.open('GET', url);
       xhr.send();
     });
   }
-};
+}
