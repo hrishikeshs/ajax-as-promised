b9f37502354ef4bb82fdc506871e927ac35a87b3
hrishikeshs <hrish2006@gmail.com>
2015-03-15 12:58:07 +0530

    Move towards a more modular structure.

--- get.js
+++ get.js
@@ -1,19 +1,17 @@
 var Promise = require("bluebird");
 
-module.exports = {
-  get:  function(url, options) {
-    var xhr = new XMLHttpRequest();
-    return new Promise(function(resolve, reject) {
-      xhr.onload = function() {
-        if(this.status === 200) {
-          resolve(this.response);
-        }
-        else {
-          reject(this.response);
-        }
-      };
-      xhr.open('GET', url);
-      xhr.send();
-    });
-  }
+module.exports = function(url, options) {
+  var xhr = new XMLHttpRequest();
+  return new Promise(function(resolve, reject) {
+    xhr.onload = function() {
+      if(this.status === 200) {
+        resolve(this.response);
+      }
+      else {
+        reject(this.response);
+      }
+    };
+    xhr.open('GET', url);
+    xhr.send();
+  });
 }
--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 var Promise = require("bluebird");
-var GET = require("get");
-/*
+var GET = require("./get");
 
-var PUT = require("put");
-var POST = require("POST");
-var DELETE = require("DELETE");
-// more verbs to be added as basic utilities here
-*/
 module.exports = {
-  request:  function(url, type, options) {
-    var xhr = new XMLHttpRequest();
-    return new Promise(function(resolve, reject) {
-      xhr.onload = function() {
-        if(this.status === 200) {
-          resolve(this.response);
-        }
-        else {
-          reject(this.response);
-        }
-      };
-      xhr.open(type, url);
-      xhr.send();
-    });
-  }
+  get: GET
 };
