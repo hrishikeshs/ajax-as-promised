0bd72f495572d2a9d850e839882a504abf7b8837
hrishikeshs <hrish2006@gmail.com>
2015-03-29 12:09:58 +0530

    Add test for successful `get`.

--- tests/get.js
+++ tests/get.js
@@ -1,3 +1,4 @@
+var get = require('../lib/get');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 var should = chai.should();
@@ -5,10 +6,18 @@ var should = chai.should();
 chai.use(chaiAsPromised);
 
 describe('get-test', function() {
-  describe('sample',  function() {
-    it('sample test to see stuff works', function() {
-      var res = new Promise(function(res, rej) { res(1); });
-      res.should.eventually.equal(1);
+  describe('promise should resolve with response',  function() {
+
+    before(function () {
+      global.XMLHttpRequest = require('xhr2');
+    });
+
+    it('should fetch json from server and resolve successfully', function() {
+      var res = get('https://github.com/hrishikeshs/ajax-as-promised/graphs/contributors-data')
+            .then(function(response) {
+              return response[0].author && response[0].author.login;
+            });
+      res.should.eventually.equal('hrishikeshs');
     });
   });
 });
